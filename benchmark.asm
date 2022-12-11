ba85f6c2b67a0c9039e62314cb4805713640a422
Rohan Rajesh Kalbag <46604893+rohankalbag@users.noreply.github.com>
2022-12-11 10:19:35 +0530

    Made some more changes

--- benchmark.asm
+++ benchmark.asm
@@ -5,7 +5,7 @@ lw r2,r0,62                 ; load in r2
 adi r3,r0,1   
 ndu r5,r1,r1                ; store 1s complement of r1 in r5
 add r5,r5,r3                ; store 2s complement of r1 in r5
-adi r3,r0,0                  ; use r3 to store result
+adi r3,r0,0                 ; use r3 to store result
 ; start iterating from here
 add r3,r2,r0                ; add the second number
 add r5,r6,r0                ; increment loop variable r5
